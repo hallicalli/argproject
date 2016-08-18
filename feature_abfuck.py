f73079936fbcaad755f249ec8c3d6d9734289d58
fabsurd <fabian.schan@me.com>
2016-08-18 10:38:08 +0200

    Update Feature
    Hab jetzt noch einige Features diazugemacht.. müsst mal schauen :)

--- feature_abfuck.py
+++ feature_abfuck.py
@@ -38,8 +38,6 @@ def modalverb(sent):
 		if token[3] == 'MD':
 			return token[1]
 
-#def keywords(item) anzahl normalisieren ueber satzlaenge
-
 def count_pronomen(sent):
 	counter = 0
 	for token in sent['tokens']:
@@ -48,7 +46,6 @@ def count_pronomen(sent):
 	return counter
 
 
-#def is_subject_pronomen???
 
 def count_satzzeichen(sent):
 	count_punct = 0
@@ -57,7 +54,79 @@ def count_satzzeichen(sent):
 			count_punct += 1
 	return count_punct
 
+def keyword_binary_eo(sent):
+	with open("keywords_eo.txt", "r") as f_in:
+		eo_bin = 0
+		f_string = f_in.read()
+		keywords_eo = f_string.split("\r\n")
+		for token in sent['tokens']:
+			if token[1] in keywords_eo:
+				eo_bin = 1
+		return eo_bin
 
+def keyword_binary_po(sent):
+	with open("keywords_po.txt", "r") as f_in:
+		po_bin = 0
+		f_string = f_in.read()
+		keywords_po = f_string.split()
+		for token in sent['tokens']:
+			if token[1] in keywords_po:
+				po_bin = 1
+		return po_bin
+
+def keyword_count_eo(sent): #NOCH NORMALISIEREN
+	with open("keywords_eo.txt", "r") as f_in:
+		eo_count = 0
+		f_string = f_in.read()
+		keywords_eo = f_string.split("\r\n")
+		for token in sent['tokens']:
+			if token[1] in keywords_eo:
+				eo_count += 1
+		sents_l = sents_length(sent)
+		norm_eo_count = eo_count / float(sents_l)
+		return norm_eo_count
+
+def keyword_count_po(sent): #NOCH NORMALISIEREN
+	with open("keywords_po.txt", "r") as f_in:
+		po_count = 0
+		f_string = f_in.read()
+		keywords_po = f_string.split("\r\n")
+		for token in sent['tokens']:
+			if token[1] in keywords_po:
+				po_count += 1
+		sents_l = sents_length(sent)
+		norm_po_count = po_count / float(sents_l)
+		return norm_po_count
+ 
+def adverb(sent):
+ 	for token in sent['tokens']:
+		if token[3] == 'RBR':
+			return token[1]
+		elif token[3] == 'WRB':
+			return token[1]
+
+ #def praemissen-claim-liste
+
+ #fremdwoerter
+
+ #def is_subject_pronomen???
+
+def iob_eo(sent):
+	iob_eo_bin = 0
+	for token in sent['tokens']:
+		if re.search(r'Expert', token[2]):
+ 			iob_eo_bin = 1
+ 	return iob_eo_bin
+
+def iob_po(sent):
+	iob_po_bin = 0
+	for token in sent['tokens']:
+		if re.search(r'Popular', token[2]):
+ 			iob_po_bin = 1
+ 	return iob_po_bin
+
+
+ #def pronomen im singular oder plural?
 
 
 
@@ -90,8 +159,17 @@ def main():
 			feature_values.append(counts_ner(sent)) #Anzahl NER geteilt durch Satzlaenge * 100 > Gibt Prozent anwenden
 			feature_values.append(modalverb(sent)) #Welches Modalverb
 			feature_values.append(mainverb(sent)) #Welches Hauptverb
+			feature_values.append(adverb(sent))
 			feature_values.append(count_pronomen(sent))
 			feature_values.append(count_satzzeichen(sent))
+			feature_values.append(keyword_binary_eo(sent))
+			feature_values.append(keyword_binary_po(sent))
+			feature_values.append(keyword_count_eo(sent))
+			feature_values.append(keyword_count_po(sent))
+			feature_values.append(iob_eo(sent))
+			feature_values.append(iob_po(sent))
+
+
 			feature_result.append(feature_values)
 
 	print feature_result
